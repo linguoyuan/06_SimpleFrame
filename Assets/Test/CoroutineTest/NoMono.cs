c784fefe4d9c17cffb6a269ccd1c3c7f90b4f5a2
林国源 <linguoyuan@dm-ai.cn>
2020-09-13 18:26:17 +0800

    add coroutine test sample

---
+++ Assets/Test/CoroutineTest/NoMono.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoMono
+{
+    public void Test()
+    {
+        CoroutineMgr.Single.ExecuteOnce(MyEnumeratorTest());
+    }
+
+    IEnumerator MyEnumeratorTest()
+    {
+        Debug.Log("使用了无mono的脚本启动了协程");
+        yield return null;
+    }
+}
+
